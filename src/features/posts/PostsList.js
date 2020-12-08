7c3febebff11de2079bf123a3870db3a4708c293
easonlau <easonlauatha@gmail.com>
2020-12-09 00:13:44 +0800

    Use thunk to fetch data

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,39 +1,61 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
 import { TimeAgo } from './TimeAgo'
 import { ReactionButtons } from './ReactionButtons'
+import { selectAllPosts, fetchPosts } from './postsSlice'
+
 
 export const PostsList = () => {
-  const posts = useSelector(state => state.posts)
+  const dispatch = useDispatch()
+  const posts = useSelector(selectAllPosts)
 
-  const orderedPosts = posts.slice().sort((a, b) => {
-    if( typeof(a.date) === 'undefined' || typeof(b.date) === 'undefined'){
-      return -1; //sort to the bottom if undefined
+  const postStatus = useSelector(state => state.posts.status)
+  const error = useSelector(state => state.posts.error)
+
+  //useEffect(()=>{}, []) componentDidMount
+  //useEffect(()=>{}, [value]) componentDidUpdate
+  useEffect(() => {
+    if (postStatus === 'idle') {
+      dispatch(fetchPosts())
     }
-    return b.date.localeCompare(a.date);
-  })
+  }, [postStatus, dispatch])
 
-  //const renderedPosts = posts.map(post => (
-  const renderedPosts = orderedPosts.map(post => {
-    return (
-      <article className="post-excerpt" key={post.id}>
-        <h3>{post.title}</h3>
-        <p className="post-content">{post.content.substring(0, 100)}</p>
-        <Link to={`/posts/${post.id}`} className="button muted-button">View</Link>
-        <Link to={`/editPost/${post.id}`} className="button muted-button">Edit</Link>
-        <PostAuthor userId={post.user} />
-        <TimeAgo timestamp={post.date}/>
-        <ReactionButtons post={post}/>
-      </article>
-    )
-  })
+  let content
+
+  if (postStatus === 'loading') {
+    content = <div className="loader">Loading...</div>
+  } else if (postStatus === 'succeeded') {
+    const orderedPosts = posts.slice().sort((a, b) => {
+      if( typeof(a.date) === 'undefined' || typeof(b.date) === 'undefined'){
+        return -1; //sort to the bottom if undefined
+      }
+      return b.date.localeCompare(a.date);
+    })
+    //const renderedPosts = posts.map(post => (
+    const renderedPosts = orderedPosts.map(post => {
+      return (
+        <article className="post-excerpt" key={post.id}>
+          <h3>{post.title}</h3>
+          <p className="post-content">{post.content.substring(0, 100)}</p>
+          <Link to={`/posts/${post.id}`} className="button muted-button">View</Link>
+          <Link to={`/editPost/${post.id}`} className="button muted-button">Edit</Link>
+          <PostAuthor userId={post.user} />
+          <TimeAgo timestamp={post.date}/>
+          <ReactionButtons post={post}/>
+        </article>
+      )
+    })
+    content = renderedPosts
+  } else if (postStatus === 'failed') {
+    content = <div>{error}</div>
+  }
 
   return (
     <section className="posts-list">
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   )
 }
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,15 +1,20 @@
-import { createSlice } from '@reduxjs/toolkit'
-import { nanoid } from '@reduxjs/toolkit'
-import { sub } from 'date-fns'
+import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit'
+import { client } from '../../api/client'
 
-const initialState = [
-  { id: '1', title: 'First Post!', content: 'Hello!', 
-  date: sub(new Date(), { minutes: 10 }).toISOString(),
-  reactions: {thumbsUp:1, hooray:2, heart:3, rocket:4, eyes:5} },
-  { id: '2', title: 'Second Post', content: 'More text', 
-  date: sub(new Date(), { minutes: 5 }).toISOString(),
-  reactions: {} }
-]
+//const initialState = [{ id: '1', title: 'First Post!', content: 'Hello!', date: sub(new Date(), { minutes: 10 }).toISOString(), reactions: {thumbsUp:1, hooray:2, heart:3, rocket:4, eyes:5} },{ id: '2', title: 'Second Post', content: 'More text', date: sub(new Date(), { minutes: 5 }).toISOString(),reactions: {} }]
+const initialState = {
+  posts: [],
+  status: 'idle',
+  error: null
+}
+
+//createAsyncThunk generates thunks that automatically dispatch those "start/success/failure" actions
+//argument1: A string that will be used as the prefix for the generated action types
+//argument2: A "payload creator" callback function that should return a Promise containing some data, or a rejected Promise with an error
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  const response = await client.get('/fakeApi/posts')
+  return response.posts
+})
 
 const postsSlice = createSlice({
   name: 'posts',
@@ -18,7 +23,7 @@ const postsSlice = createSlice({
     reactionAdded(state, action) {
       //action objects as small as possible, so calculate the new state value in reducer
       const { postId, reaction } = action.payload
-      const existingPost = state.find(post => post.id === postId)
+      const existingPost = state.posts.find(post => post.id === postId)
       if (existingPost) {
         if (existingPost.reactions[reaction] == null){
           existingPost.reactions[reaction] = 0
@@ -42,24 +47,42 @@ const postsSlice = createSlice({
         //action type 'posts/postAdded2' will be added
       },
       reducer(state, action) {
-        state.push(action.payload)
+        state.posts.push(action.payload)
       }
     },
     postAdded1(state, action) { 
       //will generate action.type 'posts/postAdded1'
       //will generate action creator, postAdded1, for dispatch(postAdded1(action.payload))
-      state.push(action.payload)
+      state.posts.push(action.payload)
     },
     postUpdated(state, action) {
         const { id, title, content } = action.payload
-        const existingPost = state.find(post => post.id === id)
+        const existingPost = state.posts.find(post => post.id === id)
         if (existingPost) {
           existingPost.title = title
           existingPost.content = content
         }
     }
+  },
+  extraReducers: { //listening for  those "start/success/failure" actions
+    [fetchPosts.pending]: (state, action) => {
+      state.status = 'loading'
+    },
+    [fetchPosts.fulfilled]: (state, action) => {
+      state.status = 'succeeded'
+      // Add any fetched posts to the array
+      state.posts = state.posts.concat(action.payload)
+    },
+    [fetchPosts.rejected]: (state, action) => {
+      state.status = 'failed'
+      state.error = action.error.message
+    }
   }
 })
 
 export const { postAdded1, postAdded2, postUpdated, reactionAdded } = postsSlice.actions
+
 export default postsSlice.reducer
+
+export const selectAllPosts = state => state.posts.posts
+export const selectPostById = (state, postId) => state.posts.posts.find(post => post.id === postId)
